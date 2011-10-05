fc656a6887d15f0ea277245ab8d71f1857a436bd
Jason Morrison <jmorrison@thoughtbot.com>
2011-10-05 11:33:50 -0400

    Initial 'bundle gem backbone-forms-rails' generation

---
+++ lib/backbone-forms-rails.rb
@@ -0,0 +1,9 @@
+require "backbone-forms-rails/version"
+
+module Backbone
+  module Forms
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/backbone-forms-rails/version.rb
@@ -0,0 +1,7 @@
+module Backbone
+  module Forms
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
